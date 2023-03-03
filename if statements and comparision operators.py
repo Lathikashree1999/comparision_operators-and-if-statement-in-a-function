0949a53bced64b946b2ba325881d2e514a12f2a3
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2023-03-03 22:21:07 +0530

    Add files via upload

---
+++ if statements and comparision operators.py
@@ -0,0 +1,16 @@
+num1=int(input("Enter num1 value="))
+num2=int(input("Enter num2 value="))
+def comparision_operator():
+    if num1==num2:
+        print(num1,"is equal to",num2)
+    if num1!=num2:
+        print(num1,"is not equal to",num2)
+    if num1<num2:
+        print(num1,"is less than",num2)
+    if num1>num2:
+        print(num1,"is greater than",num2)
+    if num1<=num2:
+        print(num1,"is less than or equal to",num2)  
+    if num1>=num2:
+        print(num1,"is greater than or equal to",num2)
+comparision_operator()       
